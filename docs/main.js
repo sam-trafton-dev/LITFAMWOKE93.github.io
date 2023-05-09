de3957c32537384933e793703de48d6e437c65f5
Sam Trafton <samatrafton@gmail.com>
2023-05-09 16:51:42 +0300

    add defer command to anime.js script

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,9 @@
 
 let el;
 
-document.addEventListener('DOMContentLoaded', function() {
-    el = document.querySelector('#profile-picture');
-});
+el = document.querySelector("#profile-picture");
 
-if (el == null) {
- alert("Selection null");
-}
+
 anime({
     targets: el,
     translateY: [
